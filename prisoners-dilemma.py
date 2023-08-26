4bdafb1d050ec75c71fcd504757b1f1a7a12de1b
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-08-26 12:46:03 +0200

    base prototype

---
+++ prisoners-dilemma.py
@@ -0,0 +1,44 @@
+"""
+Simple simulation of the prisoner's dilemma experiment by Axelrod 1984
+"""
+
+config = {
+    "rounds": 1,
+    "game": "prisoners-dilemma",
+}
+
+game_history = []
+
+# ───────────────────────────────────────────────────────────────────────────────
+
+
+def play_game(actor_1_strategy, actor_2_strategy):
+    outcome = {"actor_1": -1, "actor_2": -1}
+
+    if actor_1_strategy == "cooperate" and actor_2_strategy == "cooperate":
+        outcome["actor_1"] = 1
+        outcome["actor_2"] = 1
+    elif actor_1_strategy == "defect" and actor_2_strategy == "defect":
+        outcome["actor_1"] = 2
+        outcome["actor_2"] = 2
+    elif actor_1_strategy == "cooperate" and actor_2_strategy == "defect":
+        outcome["actor_1"] = 3
+        outcome["actor_2"] = 0
+    elif actor_1_strategy == "defect" and actor_2_strategy == "cooperate":
+        outcome["actor_1"] = 0
+        outcome["actor_2"] = 3
+
+    return outcome
+
+
+def main():
+    print("Prisoners' Dilemma")
+    print("──────────────────")
+
+    actor_1_strategy = "defect"
+    actor_2_strategy = "cooperate"
+    print("Actor 1:", actor_1_strategy)
+    print("Actor 2:", actor_2_strategy)
+
+    outcome = play_game(actor_1_strategy, actor_2_strategy)
+    print("Outcome:", outcome)
