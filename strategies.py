6352aae288c1b396e05a9181e08a46624a3b7d81
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-08-28 19:42:37 +0200

    feat: two versions of alternate strat

--- strategies.py
+++ strategies.py
@@ -24,10 +24,18 @@ def __is_first_run(prev_runs: list[tuple[str, str]]) -> bool:
 # ──────────────────────────────────────────────────────────────────────────────
 
 
-def alternate(self_id: int, prev_runs: list[tuple[str, str]]) -> str:
-    """Alternate between cooperating and defecting."""
+def alternate_a(self_id: int, prev_runs: list[tuple[str, str]]) -> str:
+    """Alternate between cooperating and defecting. Start with cooperating."""
     if __is_first_run(prev_runs):
         return "cooperate"
+    last_own_action = prev_runs[-1][self_id]
+    return "defect" if last_own_action == "cooperate" else "cooperate"
+
+
+def alternate_b(self_id: int, prev_runs: list[tuple[str, str]]) -> str:
+    """Alternate between cooperating and defecting. Start with defecting."""
+    if __is_first_run(prev_runs):
+        return "defect"
     last_own_action = prev_runs[-1][self_id]
     return "defect" if last_own_action == "cooperate" else "cooperate"
 
@@ -49,8 +57,11 @@ def unforgiving(self_id: int, prev_runs: list[tuple[str, str]]) -> str:
 
 
 def opportunist(self_id: int, prev_runs: list[tuple[str, str]]) -> str:
-    """Defect if the opponent has always cooperated recently."""
+    """Defect if the opponent has always cooperated recently (%s runs)."""
     min_runs = 2  # number of runs considered as "recently"
+    # HACK have the function modify its own docstring
+    opportunist.__doc__ = str(opportunist.__doc__).replace("%s", str(min_runs))
+
     is_first_few_runs = len(prev_runs) > min_runs
     if is_first_few_runs:
         return "cooperate"
@@ -66,7 +77,8 @@ strategy_funcs = {
     "always_cooperate": lambda *_: "cooperate",
     "always_defect": lambda *_: "defect",
     "random": lambda *_: random.choice(["cooperate", "defect"]),
-    "alternate": alternate,
+    "alternate_a": alternate_a,
+    "alternate_b": alternate_b,
     "tit_for_tat": tit_for_tat,
     "unforgiving": unforgiving,
     "opportunist": opportunist,
