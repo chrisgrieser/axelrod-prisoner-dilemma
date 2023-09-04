f4ffb433c4983b296a7f38a6c5ba95cc0c65fe25
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2023-09-04 13:57:27 +0200

    refactor: cleaner preview function

--- prisoner_dilemma/preview.py
+++ prisoner_dilemma/preview.py
@@ -1,20 +1,25 @@
 """Preview Functions."""
 
+
 def preview_html(html: str) -> None:
     """HTML a preview of the output.
 
     On macOS, displayed via Quicklook, otherwise the output is written to a file.
     """
-    import os
     import platform
     from pathlib import Path
 
-    filename = ".out.html"
-    with Path(filename).open("w") as file:
+    # write to file
+    outfile = Path(".out.html")
+    with outfile.open("w") as file:
         file.write(html)
 
-
+    # on macOS: open
     if platform.system() == "Darwin":
-        os.system(f"qlmanage -p '{filename}' &>/dev/null")
+        import os
+
+        # tempfile does not work with `qlmanage` for some reason
+        os.system(f"qlmanage -p '{outfile.name}' &>/dev/null")
+        outfile.unlink()  # remove file
     else:
-        print(f"Output created as '{filename}'.")
+        print(f"Output created as '{outfile.name}'.")
